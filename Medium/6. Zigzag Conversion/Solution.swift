96db849f8276c0eb09c8b85581f6965f285ef062
Abhinav Kumar Roy <abhinav.roy23@gmail.com>
2024-08-11 18:06:29 -0700

    Update Solution.swift

--- Medium/6. Zigzag Conversion/Solution.swift
+++ Medium/6. Zigzag Conversion/Solution.swift
@@ -1,39 +1,57 @@
-class Solution {
-    func convert(_ s: String, _ numRows: Int) -> String {
+func convert(_ s: String, _ numRows: Int) -> String {
 
-        if numRows == 1 { return s }
+    if numRows == 1 { return s }
 
-        var input = Array(s)
-        var maxIndex = numRows-1
-        var indexArray : [(Int, Int)] = []
-        
-        var j = 0
-        while !input.isEmpty {
-            if j % maxIndex == 0 {
-                for i in 0...maxIndex {
-                    if !input.isEmpty {
-                        let temp = input.removeFirst()
-                        indexArray.append((i,j))
-                    }
+    var input = Array(s)
+    var maxIndex = numRows-1
+    var indexArray : [(Int, Int)] = []
+    
+    var j = 0
+    while !input.isEmpty {
+        if j % maxIndex == 0 {
+            for i in 0...maxIndex {
+                if !input.isEmpty {
+                    let temp = input.removeFirst()
+                    indexArray.append((i,j))
                 }
-            } else {
-                let temp = input.removeFirst()
-                indexArray.append(((maxIndex - (j % maxIndex)),j))
             }
-            j += 1
+        } else {
+            let temp = input.removeFirst()
+            indexArray.append(((maxIndex - (j % maxIndex)),j))
         }
-        
-        var newInput = s
-        var resultArray = Array(repeating: Array(repeating: "", count: j), count: numRows)
-        
-        for (left, right) in indexArray {
-            resultArray[left][right] = String(newInput.removeFirst())
-        }
-        
-        let resultString = resultArray.reduce("") { partialResult, tempArr in
-            return partialResult + tempArr.joined()
-        }
-        
-        return resultString
+        j += 1
     }
+    
+    var newInput = s
+    var resultArray = Array(repeating: Array(repeating: "", count: j), count: numRows)
+    
+    for (left, right) in indexArray {
+        resultArray[left][right] = String(newInput.removeFirst())
+    }
+    
+    let resultString = resultArray.reduce("") { partialResult, tempArr in
+        return partialResult + tempArr.joined()
+    }
+    
+    return resultString
+}
+
+// Optimized solution
+func convert1(_ s: String, _ numRows: Int) -> String {
+    
+    guard numRows > 1 else { return s }
+    
+    var result = Array(repeating: "", count: numRows)
+    var row = 0, down = false
+    
+    for char in s {
+        result[row] += String(char)
+        if row == 0 || row == numRows - 1 {
+            down.toggle()
+        }
+        row += down ? 1 : -1
+    }
+
+    print(result.joined())
+    return ""
 }
